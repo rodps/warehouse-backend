a28c8f1230d5b9294f22b3c8a665f94a64c2aaea
Jefferson Mantovani <jmantovani@alunos.utfpr.edu.br>
2018-07-05 17:25:54 -0300

    alteração na rota de orcamentos para permitir edição.

--- routes/orcamentos.js
+++ routes/orcamentos.js
@@ -5,21 +5,13 @@ const verifyToken = require("../middleware/index").verifyToken;
 
 // listar
 
-<<<<<<< HEAD
 router.get("/:idSolicitacao", verifyToken, function(req, res) {
-=======
-router.get("/:idSolicitacao", function(req, res) {
->>>>>>> master
   models.orcamentos
     .findAll({
       where: { solicitacao_id: req.params.idSolicitacao }
     })
     .then(orcamentos => {
-<<<<<<< HEAD
       res.status(200).json(orcamentos);
-=======
-      res.status(201).send(orcamentos);
->>>>>>> master
     })
     .catch(err => {
       res.status(400).send(err);
@@ -28,31 +20,10 @@ router.get("/:idSolicitacao", function(req, res) {
 
 //criar
 router.post("/:idSolicitacao", (req, res) => {
-<<<<<<< HEAD
   //var orcamentos = req.body;
   // console.log(orcamentos);
   console.log(req.body.id);
   if (!req.body.id) {
-=======
-  if (req.body.id) {
-    models.orcamentos.findById(req.body.id).then(orcamento => {
-      orcamento
-        .update({
-          origem: req.body.origem,
-          valor: req.body.valor,
-          cnpj_fornecedor: req.body.cnpj_fornecedor,
-          nome_fornecedor: req.body.nome_fornecedor,
-          solicitacao_id: req.params.idSolicitacao
-        })
-        .then(() => {
-          res.status(200).send("ok");
-        })
-        .catch(err => {
-          res.status(400).send(err);
-        });
-    });
-  } else {
->>>>>>> master
     models.orcamentos
       .create({
         origem: req.body.origem,
@@ -61,18 +32,12 @@ router.post("/:idSolicitacao", (req, res) => {
         nome_fornecedor: req.body.nome_fornecedor,
         solicitacao_id: req.params.idSolicitacao
       })
-<<<<<<< HEAD
       .then(response => {
         res.status(201).json(response);
-=======
-      .then(() => {
-        res.status(200).send("ok");
->>>>>>> master
       })
       .catch(err => {
         res.status(400).send(err);
       });
-<<<<<<< HEAD
   } else {
     models.orcamentos.findById(req.body.id).then(orcamento => {
       orcamento
@@ -89,8 +54,6 @@ router.post("/:idSolicitacao", (req, res) => {
           res.status(400).send(err);
         });
     });
-=======
->>>>>>> master
   }
 });
 
